ffd89d2e1816d035f665b0554b7771410af6a579
longhaoran <longhr@wfl-ischool.cn>
2019-11-16 23:44:18 +0800

    demo

--- vue/webpack-demo/src/index.js
+++ vue/webpack-demo/src/index.js
@@ -1,11 +1,16 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import printMe from './print.js';
 import './style.css';
+import {cube} from './math.js';
 function component() {
   var element = document.createElement('div');
   var btn = document.createElement('button');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n');
   btn.innerHTML = 'Click me and check the console';
   btn.onclick = printMe;
   element.appendChild(btn);
