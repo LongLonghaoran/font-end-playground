8aa45953246152d514943b5358b8a02a401f5f15
longhaoran <longhr@wfl-ischool.cn>
2019-11-16 22:07:15 +0800

    demo

--- vue/webpack-demo/src/index.js
+++ vue/webpack-demo/src/index.js
@@ -1,17 +1,15 @@
 import _ from 'lodash';
-import './style.css';
-import logo from './logo.png';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
 
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  btn.innerHTML = 'Click me and check the console';
+  btn.onclick = printMe;
+  element.appendChild(btn);
 
-  var myIcon = new Image();
-  myIcon.src = logo;
-  element.appendChild(myIcon);
   return element;
 }
 
